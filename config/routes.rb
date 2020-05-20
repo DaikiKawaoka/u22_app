1ef0a808dcc0d440b9e9cdd48b86097b57e3527b
DaikiKawaoka <kawadaiki@icloud.com>
2020-05-20 19:34:41 +0900

    rails_t_7

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  # GET	/users	index	users_path すべてのユーザーを一覧するページ
+  # GET	/users/1	show	user_path(user) 特定のユーザーを表示するページ
+  # GET	/users/new	new	new_user_path ユーザーを新規作成するページ (ユーザー登録)
+  # POST	/users	create	users_path ユーザーを作成するアクション
+  # GET	/users/1/edit	edit	edit_user_path(user) id=1のユーザーを編集するページ
+  # PATCH	/users/1	update	user_path(user) ユーザーを更新するアクション
+  # DELETE	/users/1	destroy	user_path(user) ユーザーを削除するアクション
 end
