3e8f454a170cdffb361d0b5df2cdd56807ba91c4
DaikiKawaoka <kawadaiki@icloud.com>
2020-05-25 11:29:21 +0900

    サンプルユーザ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             user_name: "aaa",
+             sex: 1,
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+# 追加のユーザーをまとめて生成する
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  user_name = Faker::Name.name
+  password = "password"
+  sex = 1
+  User.create!(name:  name,
+               email: email,
+               user_name: user_name,
+               sex: sex,
+               password:              password,
+               password_confirmation: password)
+end
