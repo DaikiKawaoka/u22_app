ffb0d0485a4da41239d6c48d9bc979708dfc8e71
DaikiKawaoka <kawadaiki@icloud.com>
2020-05-28 09:41:24 +0900

    testcomment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index,:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
-
+#test
   def index
     
   end
