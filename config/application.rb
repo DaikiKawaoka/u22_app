01e056b378d15bb4579a5eb4053d6eec4e4b6fe0
DaikiKawaoka <kawadaiki@icloud.com>
2020-06-05 18:09:30 +0900

    Ajaxの同期,フォロー機能完成

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,8 @@ module U22App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
