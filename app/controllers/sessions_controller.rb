fb712d056e40ad0d3d4e3dca378d015c36480a90
DaikiKawaoka <kawadaiki@icloud.com>
2020-06-18 13:28:30 +0900

    error対処

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,7 @@ class SessionsController < ApplicationController
       flash[:danger] = 'ログイン失敗'
       redirect_to root_url
     end
+  end
     def destroy
       log_out if logged_in?
       redirect_to root_url
